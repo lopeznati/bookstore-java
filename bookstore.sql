33e0813e94861a04bd697b396a70d7b4c16faeb1
mailen <MCaste@github.com>
2016-07-06 17:00:34 -0300

    BD

---
+++ bookstore.sql
@@ -0,0 +1,240 @@
+# SQL Manager 2005 Lite for MySQL 3.7.0.1
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : bookstore
+
+
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE DATABASE `bookstore`
+    CHARACTER SET 'latin1'
+    COLLATE 'latin1_swedish_ci';
+
+#
+# Structure for the `autores` table : 
+#
+
+CREATE TABLE `autores` (
+  `id` int(11) NOT NULL auto_increment,
+  `nombre` varchar(20) default NULL,
+  `apellido` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `categorias` table : 
+#
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL auto_increment,
+  `descripcion` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `localidades` table : 
+#
+
+CREATE TABLE `localidades` (
+  `id` int(11) NOT NULL auto_increment,
+  `nombre` varchar(20) default NULL,
+  `codigo_postal` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `clientes` table : 
+#
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL auto_increment,
+  `usuario` varchar(20) default NULL,
+  `clave` varchar(20) default NULL,
+  `nombre` varchar(20) default NULL,
+  `apellido` varchar(20) default NULL,
+  `telefono` varchar(20) default NULL,
+  `mail` varchar(20) default NULL,
+  `rol` varchar(20) default NULL,
+  `foto` varchar(20) default NULL,
+  `id_localidad` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `id_localidad` (`id_localidad`),
+  CONSTRAINT `clientes_fk` FOREIGN KEY (`id_localidad`) REFERENCES `localidades` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `editoriales` table : 
+#
+
+CREATE TABLE `editoriales` (
+  `id` int(11) NOT NULL auto_increment,
+  `nombre` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `libros` table : 
+#
+
+CREATE TABLE `libros` (
+  `id` int(11) NOT NULL auto_increment,
+  `isbn` int(11) default NULL,
+  `titulo` varchar(20) default NULL,
+  `sipnosis` text,
+  `numero_edicion` int(11) default NULL,
+  `cantidad_paginas` int(11) default NULL,
+  `precio` double(15,3) default NULL,
+  `existencia` int(11) default NULL,
+  `foto` varchar(20) default NULL,
+  `id_editorial` int(11) default NULL,
+  `id_categoria` int(11) default NULL,
+  `id_autor` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `id_editorial` (`id_editorial`),
+  KEY `id_categoria` (`id_categoria`),
+  KEY `id_autor` (`id_autor`),
+  CONSTRAINT `libros_fk` FOREIGN KEY (`id_editorial`) REFERENCES `editoriales` (`id`),
+  CONSTRAINT `libros_fk1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`),
+  CONSTRAINT `libros_fk2` FOREIGN KEY (`id_autor`) REFERENCES `autores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `tipos_tarrjetas` table : 
+#
+
+CREATE TABLE `tipos_tarrjetas` (
+  `id` int(11) NOT NULL auto_increment,
+  `nombre` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `tarjetas` table : 
+#
+
+CREATE TABLE `tarjetas` (
+  `id` int(11) NOT NULL auto_increment,
+  `numero_tarjeta` varchar(20) default NULL,
+  `titular` varchar(50) default NULL,
+  `codigo_seguridad` int(11) default NULL,
+  `id_tipo_tarjeta` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `id_tipo_tarjeta` (`id_tipo_tarjeta`),
+  CONSTRAINT `tarjetas_fk` FOREIGN KEY (`id_tipo_tarjeta`) REFERENCES `tipos_tarrjetas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `pedidos` table : 
+#
+
+CREATE TABLE `pedidos` (
+  `id` int(11) NOT NULL auto_increment,
+  `fecha_pedido` date default NULL,
+  `cantidad_libro` int(11) default NULL,
+  `domicilio` varchar(50) default NULL,
+  `subtotal` double(15,3) default NULL,
+  `id_tarjeta` int(11) default NULL,
+  `id_libro` int(11) default NULL,
+  `id_cliente` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `id_tarjeta` (`id_tarjeta`),
+  KEY `id_libro` (`id_libro`),
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `pedidos_fk2` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
+  CONSTRAINT `pedidos_fk` FOREIGN KEY (`id_tarjeta`) REFERENCES `tarjetas` (`id`),
+  CONSTRAINT `pedidos_fk1` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for the `autores` table  (LIMIT 0,500)
+#
+
+INSERT INTO `autores` (`id`, `nombre`, `apellido`) VALUES 
+  (1,'Pablo ','Coelho'),
+  (2,'Cassandra','Claire');
+
+COMMIT;
+
+#
+# Data for the `categorias` table  (LIMIT 0,500)
+#
+
+INSERT INTO `categorias` (`id`, `descripcion`) VALUES 
+  (1,'terror'),
+  (2,'ciencia ficcion');
+
+COMMIT;
+
+#
+# Data for the `localidades` table  (LIMIT 0,500)
+#
+
+INSERT INTO `localidades` (`id`, `nombre`, `codigo_postal`) VALUES 
+  (1,'Rosario',2000),
+  (2,'Clarke',2218);
+
+COMMIT;
+
+#
+# Data for the `clientes` table  (LIMIT 0,500)
+#
+
+INSERT INTO `clientes` (`id`, `usuario`, `clave`, `nombre`, `apellido`, `telefono`, `mail`, `rol`, `foto`, `id_localidad`) VALUES 
+  (1,'mcaste','1234','Mailen','Castellarin','3476565656','mc@gmail.com','admin',NULL,2),
+  (2,'nlopez','9876','Natali',NULL,NULL,NULL,NULL,NULL,NULL);
+
+COMMIT;
+
+#
+# Data for the `editoriales` table  (LIMIT 0,500)
+#
+
+INSERT INTO `editoriales` (`id`, `nombre`) VALUES 
+  (1,'Ediciones B'),
+  (2,'Montana');
+
+COMMIT;
+
+#
+# Data for the `libros` table  (LIMIT 0,500)
+#
+
+INSERT INTO `libros` (`id`, `isbn`, `titulo`, `sipnosis`, `numero_edicion`, `cantidad_paginas`, `precio`, `existencia`, `foto`, `id_editorial`, `id_categoria`, `id_autor`) VALUES 
+  (1,1000,'Adulterio','Linda está casada con un hombre rico, tienen dos hijos y la familia vive en una hermosa casa en Ginebra, Suiza. Trabaja en el periódico más importante del país, es guapa, viste bien y tiene todo lo que se pueda desear. A ojos de todos, su vida es perfecta. Sin embargo, no es feliz; una gran insatisfacción la corroe y se siente culpable por no ser capaz de disfrutar de lo que tiene. Por eso no habla con nadie de lo que sucede. Ama a su marido pero la relación con él se ha vuelto rutinaria, apática.\r\n\r\nUn día, el periódico la envía a entrevistar a Jacob König, un antiguo novio del instituto que ahora es un político de cierta relevancia. Este encuentro es suficiente para que ella se sienta capaz de hacer algo con lo que soñaba desde muchacha, y empieza a dar rienda suelta a sus fantasías. Vuelve a sentir pasión por la vida. Ahora hará todo lo que sea para conquistar ese amor imposible y descenderá hasta el fondo del pozo de las emociones humanas para, por fin, encontrar su redención.\r\n',1000,5000,300,5,NULL,1,1,NULL);
+
+COMMIT;
+
+#
+# Data for the `tipos_tarrjetas` table  (LIMIT 0,500)
+#
+
+INSERT INTO `tipos_tarrjetas` (`id`, `nombre`) VALUES 
+  (1,'Naranja'),
+  (2,'Mastercard'),
+  (3,'Visa'),
+  (4,'American Express'),
+  (5,'Nativa');
+
+COMMIT;
+
+#
+# Data for the `tarjetas` table  (LIMIT 0,500)
+#
+
+INSERT INTO `tarjetas` (`id`, `numero_tarjeta`, `titular`, `codigo_seguridad`, `id_tipo_tarjeta`) VALUES 
+  (1,'12345678','Natali Lopez Leiva',1234,1),
+  (2,'87654321','Mailen Castellarin',2933,2),
+  (3,'66666666',NULL,NULL,NULL);
+
+COMMIT;
+
+#
+# Data for the `pedidos` table  (LIMIT 0,500)
+#
+
+INSERT INTO `pedidos` (`id`, `fecha_pedido`, `cantidad_libro`, `domicilio`, `subtotal`, `id_tarjeta`, `id_libro`, `id_cliente`) VALUES 
+  (1,'2016-05-20',2,'Rosario',100,1,1,1);
+
+COMMIT;
+
